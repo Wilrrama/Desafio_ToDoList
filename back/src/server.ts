e0330db459969cc390d52218268530bd438ec632
wilrrama <wilrrama@hotmail.com>
2024-08-27 20:34:48 -0300

    feat: database connection

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,15 @@
 import app from "./app";
+import "dotenv/config";
+import { AppDataSource } from "./data-source";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("database is running");
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
