ef4344eaa0edecf613659eb705fc37f31c32a2bb
wilrrama <wilrrama@hotmail.com>
2024-08-30 08:50:11 -0300

    feat: middleware Is Owner

---
+++ back/src/middlewares/ensureIsOwner.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Task } from "../entities/tasks.entity";
+import { AppError } from "../errors/AppError";
+import exp from "constants";
+
+const ensureIsOwnerMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const taskRepository = AppDataSource.getRepository(Task);
+  const taskId = req.params.id;
+  const userId = res.locals.userId;
+
+  const task = await taskRepository.findOne({
+    where: {
+      id: taskId,
+    },
+    relations: {
+      user: true,
+    },
+  });
+
+  if (!task) {
+    throw new AppError("task not found", 404);
+  }
+
+  if (task.user.id != userId) {
+    throw new AppError("You don´t have permission", 403);
+  }
+
+  return next();
+};
+
+export { ensureIsOwnerMiddleware };
