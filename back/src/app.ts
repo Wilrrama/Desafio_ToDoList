eb16fb840b161727141a0e726aff0a3a94d62299
wilrrama <wilrrama@hotmail.com>
2024-08-31 21:07:08 -0300

    docs: swagger

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,6 +1,8 @@
 import "express-async-errors";
 import "reflect-metadata";
 import express from "express";
+import swaggerUiExpress from "swagger-ui-express";
+import swagerDocument from "./swagger.json";
 import { usersRoutes } from "./routes/users.route";
 import { handleAppErrorMiddleware } from "./middlewares/handleAppError.middleware";
 import { sessionRouter } from "./routes/session.route";
@@ -11,6 +13,12 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => res.json("Minha primeira vaga Dev, Desafio To Do"));
+
+app.use(
+  "/api-doc",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swagerDocument)
+);
 
 app.use("/users", usersRoutes);
 app.use("/login", sessionRouter);
